fe8109b00c3c8fe6a4aa5bd8a4421ed44ea8b211
David Hamill <dasco597@gmail.com>
2019-07-05 15:31:17 +0100

    draw indiviual pixels

--- Cedai_Engine/src/c_sdl.cpp
+++ Cedai_Engine/src/c_sdl.cpp
@@ -4,26 +4,22 @@
 #include "c_sdl.h"
 #include "opencl.h"
 
+const int SCREEN_WIDTH  = 640;
+const int SCREEN_HEIGHT = 480;
+
 void run_sdl() {
-	
-	// start up sdl
+
 	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
 		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
 		return;
 	}
 
-	// create a window with title "Hello World" at 100, 100 on the screen with w:640 h:480 and show it
-	SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+	SDL_Window *win = SDL_CreateWindow("Cedai", 100, 100, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 	if (win == nullptr) {
 		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
 		return;
 	}
 
-	// create a renderer that will draw to the window, -1 specifies that we want to load whichever
-	// video driver supports the flags we're passing
-	// flags: SDL_RENDERER_ACCELERATED: We want to use hardware accelerated rendering
-	// SDL_RENDERER_PRESENTVSYNC: We want the renderer's present function (update screen) to be
-	// synchronized with the monitor's refresh rate
 	SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (ren == nullptr) {
 		SDL_DestroyWindow(win);
@@ -32,46 +28,31 @@ void run_sdl() {
 		return;
 	}
 
-	// SDL 2.0 now uses textures to draw things but SDL_LoadBMP returns a surface
-	// this lets us choose when to upload or remove textures from the GPU
-	SDL_Surface *bmp = SDL_LoadBMP("hello.bmp");
-	if (bmp == nullptr) {
-		SDL_DestroyRenderer(ren);
-		SDL_DestroyWindow(win);
-		std::cout << "SDL_LoadBMP Error: " << SDL_GetError() << std::endl;
-		SDL_Quit();
-		return;
-	}
-
-	// to use a hardware accelerated texture for rendering we can create one from
-	// the surface we loaded
-	SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, bmp);
-	SDL_FreeSurface(bmp); // we no longer need the surface
-	if (tex == nullptr) {
-		SDL_DestroyRenderer(ren);
-		SDL_DestroyWindow(win);
-		std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
-		SDL_Quit();
-		return;
-	}
-
-	// a sleepy rendering loop, wait for 3 seconds and render and present the screen each time
-	for (int i = 0; i < 3; ++i) {
+	//Our event structure
+	SDL_Event e;
+	bool quit = false;
+	while (!quit) {
 		// clear the renderer
 		SDL_RenderClear(ren);
-		// draw the texture
-		SDL_RenderCopy(ren, tex, NULL, NULL);
+		// draw pixels
+		for (int x = 0; x < SCREEN_WIDTH; x++) {
+			for (int y = 0; y < SCREEN_HEIGHT; y++) {
+				SDL_SetRenderDrawColor(ren, 255, 255 * x / SCREEN_WIDTH, 255 * y / SCREEN_HEIGHT, 255);
+				SDL_RenderDrawPoint(ren, x, y);
+			}
+		}
 		// update the screen
 		SDL_RenderPresent(ren);
-		// take a quick break after all that hard work
-		SDL_Delay(1000);
+
+		// process pending events
+		while (SDL_PollEvent(&e)) {
+			if (e.type == SDL_QUIT || e.type == SDL_KEYDOWN || e.type == SDL_MOUSEBUTTONDOWN)
+				quit = true;
+		}
 	}
 
 	// clean up our objects and quit
-	SDL_DestroyTexture(tex);
 	SDL_DestroyRenderer(ren);
 	SDL_DestroyWindow(win);
 	SDL_Quit();
-	
-	//return 0;
 }
